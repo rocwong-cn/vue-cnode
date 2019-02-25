6b823b0a9cf90b4dd60d29b642d92b1d050e29ed
peng.wang <wangpeng16@100tal.com>
2019-02-25 11:14:18 +0800

    first commit

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 Vue.use(Router);
+Vue.use(VueAxios, axios);
 
 export default new Router({
   mode: 'history',
